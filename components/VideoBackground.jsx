448c7dd30c53759e64ac223cad0fabd9cd6f6610
Alessandro Stella <alessandro.stella2004@gmail.com>
2022-07-09 10:05:30 +0200

    First commit

---
+++ components/VideoBackground.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import styles from "../styles/VideoBackground.module.css";
+
+export default function VideoBackground() {
+    const [bottom, setBottom] = useState(0);
+
+    useEffect(() => {
+        const parallaxEffect = () => {
+            setBottom(Math.floor(window.scrollY / 20));
+        };
+
+        window.addEventListener("scroll", parallaxEffect);
+
+        return () => {
+            window.removeEventListener("scroll", parallaxEffect);
+        };
+    }, []);
+
+    return (
+        <div
+            className={styles.background}
+            style={{ transform: `translateY(-${bottom}vh)` }}>
+            <video src="/videoBg.mp4" autoPlay muted></video>
+            <img src="/untitled.png" alt="Background" />
+
+            <div className={styles.arrowDown}></div>
+        </div>
+    );
+}
---
+++ pages/index.jsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+import VideoBackground from "../components/VideoBackground";
+import Loader from "../components/Loader";
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Sup3r_ Portfolio</title>
+                <meta
+                    name="description"
+                    content="A personal portfolio containing some of the past projects created by the 3d designer Sup3r_"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <VideoBackground />
+
+            <div className={styles.mainPage}>
+                <Loader />
+            </div>
+        </div>
+    );
+}
