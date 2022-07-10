3a0f50fc1627dfc58481413cd3d2ccea942c29aa
Alessandro Stella <alessandro.stella2004@gmail.com>
2022-07-10 17:29:18 +0200

    site update

--- build/static/chunks/webpack.js
+++ build/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "961fda3b4e3cf441"; }
+/******/ 		__webpack_require__.h = function() { return "03f9c4ba58341844"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
