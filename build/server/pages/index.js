3d7f3966b42d3da5d8ecd54b4262d5b1743df2cc
Alessandro Stella <alessandro.stella2004@gmail.com>
2022-07-20 03:31:43 +0200

    site update

--- build/server/pages/index.js
+++ build/server/pages/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54e13ce6b5c1b6a723ab6afe907d90e4c1231bb35a687e56acf21765b5b6b5c0
-size 149018
+oid sha256:3707ab9db54b64be7da1256d02d503ca298fa00a421c4993bd95e9e600866bc1
+size 148190
